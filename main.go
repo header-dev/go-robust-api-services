fd8981aed8f752354d6d9ebf9cce81165a49f387
kritawit <kritawit@tisco.co.th>
2024-11-13 11:16:54 +0700

    [Feature] complete create todos and migration

--- main.go
+++ main.go
@@ -1,13 +1,67 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"header-dev/go-robust-api-services/todo"
+
+	"github.com/gin-gonic/gin"
+
+	"database/sql"
+
+	_ "modernc.org/sqlite"
+)
+
+var db *sql.DB
+
+func initDatabase(dbPath string) error {
+	var err error
+	db, err = sql.Open("sqlite", dbPath)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createTodosTable() error {
+	createTableSQL := `CREATE TABLE IF NOT EXISTS todos (
+        "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT,		
+        "title" TEXT
+    );`
+	_, err := db.Exec(createTableSQL)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
+	dbPath := "test.db"
+	if len(dbPath) == 0 {
+		panic("specify the SQLITE_DB_PATH environment variable")
+	}
+	err := initDatabase(dbPath)
+
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic("error initializing DB connection: ping error: ")
+	}
+
+	//Auto migration
+	createTodosTable()
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	handler := todo.NewTodoHandler(db)
+	r.POST("/todos", handler.NewTask)
+
 	r.Run()
 }
--- todo/todo.go
+++ todo/todo.go
@@ -1,1 +1,117 @@
 package todo
+
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Todo struct {
+	ID    int64  `json:"id"`
+	Title string `json:"text"`
+}
+
+type TodoHandler struct {
+	db *sql.DB
+}
+
+func NewTodoHandler(db *sql.DB) *TodoHandler {
+	return &TodoHandler{db: db}
+}
+
+func (t *TodoHandler) NewTask(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	query := "INSERT INTO todos (title) VALUES (?)"
+	result, err := t.db.Exec(query, todo.Title)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	todo.ID = id
+	c.JSON(http.StatusCreated, gin.H{
+		"ID": todo.ID,
+	})
+}
+
+func (t *TodoHandler) GetTask(c *gin.Context) {
+	id := c.Param("id")
+	var todo Todo
+
+	query := "SELECT id, title FROM todos WHERE id = ?"
+	err := t.db.QueryRow(query, id).Scan(&todo.ID, &todo.Title)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{
+				"error": "Todo not found",
+			})
+		} else {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"error": err.Error(),
+			})
+		}
+		return
+	}
+
+	c.JSON(http.StatusOK, todo)
+}
+
+func (t *TodoHandler) UpdateTask(c *gin.Context) {
+	id := c.Param("id")
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	query := "UPDATE todos SET title = ? WHERE id = ?"
+	_, err := t.db.Exec(query, todo.Title, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Todo updated successfully",
+	})
+}
+
+func (t *TodoHandler) DeleteTask(c *gin.Context) {
+	id := c.Param("id")
+
+	query := "DELETE FROM todos WHERE id = ?"
+	_, err := t.db.Exec(query, id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Todo deleted successfully",
+	})
+}
